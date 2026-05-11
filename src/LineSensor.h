fcc324777f0026c8b96f4bb13a8db88f4cd69965
Aditya <aditya.kandhai@gmail.com>
2026-05-11 21:20:48 +0200

    begin .h file

---
+++ src/LineSensor.h
@@ -0,0 +1,25 @@
+#pragma once //header guard voor beveiliging, wanneer deze header file opgeroepen wordt in andere klassen
+#include <Wire.h>
+#include <Zumo32U4.h>
+//
+Zumo32U4LineSensors LineSensor;
+
+class LineSensor{
+public:
+LineSensor();
+~LineSensor();
+
+void initialize();
+void calibrateLineSensors();
+void printReadingsToSerial();
+bool isCalibrated() const;
+char getLastError() const;
+
+private:
+Zumo32U4LineSensors sensors;
+
+
+
+
+
+};
